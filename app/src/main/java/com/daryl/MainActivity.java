6935428d9db2d7de0b102b5eb4a119960a19631d
TristanPinaudeau <tristan@flexfactory.fr>
2019-03-23 19:19:00 +0100

    Add basic navigation to activity zombie

--- app/src/main/java/com/daryl/MainActivity.java
+++ app/src/main/java/com/daryl/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }
 
+        final ImageButton button = findViewById(R.id.zombie_spotter);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent zombieSpotter = new Intent(MainActivity.this, ZombieSpotterActivity.class);
+                startActivity(zombieSpotter);
+                System.out.println("test ok");
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/daryl/ZombieSpotterActivity.java
@@ -0,0 +1,15 @@
+package com.daryl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZombieSpotterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zombie_spotter);
+
+    }
+
+}
